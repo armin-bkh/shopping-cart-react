71a13e2dfad70347c574047563f89ee5433a384e
armin bakhshi <arminbkh0921@yahoo.com>
2021-11-10 14:47:40 +0330

    develope HomePage and Layout components

---
+++ src/Layouts/Layout.js
@@ -0,0 +1,11 @@
+const Layout = ({children}) => {
+  return (
+    <>
+      <header>naviagtion</header>
+      {children}
+      <footer>footer</footer>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../Layouts/Layout";
+
+const HomePage = () => {
+    return ( 
+        <Layout>
+            shopping app
+        </Layout>
+     );
+}
+ 
+export default HomePage;
