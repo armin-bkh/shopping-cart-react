01c81bee9be1b31683c50be18d64465e3d486ea1
armin bakhshi <arminbkh0921@yahoo.com>
2021-11-11 09:57:25 +0330

    update ui design Navigation component

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,25 +1,28 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navigation.module.scss";
+import { AiFillHome } from "react-icons/ai";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
 const navigation = [
-    { to: "/", title: "home" },
-    { to: "/cart", title: "cart" },
+  { to: "/", title: "home", icon: <AiFillHome /> },
+  { to: "/cart", title: "cart", icon: <HiOutlineShoppingCart /> },
 ];
 
 const Navigation = () => {
   return (
     <header>
       <nav className={styles.nav}>
-          <h1 className={styles.title}>Armin shopping</h1>
+        <h1 className={`iconTxt ${styles.title}`}>Armin shopping</h1>
         <ul className={styles.navBar}>
           {navigation.map((nav) => (
             <li key={nav.to}>
               <NavLink
-                className={(link) =>
-                  link.isActive ? styles.active : styles.navItem
+                className={({ isActive }) =>
+                  `headers ${styles.navItem} ` + (isActive ? styles.active : "")
                 }
                 to={nav.to}
               >
+                <span className={styles.icon}>{nav.icon}</span>
                 {nav.title}
               </NavLink>
             </li>
